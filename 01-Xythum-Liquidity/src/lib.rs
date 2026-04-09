9c8e86f745de8845fb70d72a8cb26b7049ef1ee2
ramanarolla1-source <ramana.rolla1@gmail.com>
2026-04-09 09:34:24 +0530

    Create lib.rs

---
+++ 01-Xythum-Liquidity/src/lib.rs
@@ -0,0 +1,5 @@
+sset Vaulting: Functions for agents to deposit/withdraw collateral.
+
+Liquidity Logic: How Xythum calculates the available "buying power" for an agent based on their RWA (Real World Asset) holdings.
+
+Mint/Burn/Transfer: Standard functions for moving "Agent Credits" or stablecoins.
